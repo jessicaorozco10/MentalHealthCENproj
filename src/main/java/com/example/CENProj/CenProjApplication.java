328f0d077b6f396b46ad79072dbb988ffe335bd2
jessicaorozco10 <jessicaorozcorodriguez@gmail.com>
2025-01-19 12:35:39 -0500

    first commit

---
+++ src/main/java/com/example/CENProj/CenProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.CENProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CenProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CenProjApplication.class, args);
+	}
+
+}
