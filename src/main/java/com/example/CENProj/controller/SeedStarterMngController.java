10117f29f73227c93201376b0f99b06d245b1106
jessicaorozco10 <jessicaorozcorodriguez@gmail.com>
2025-02-09 15:20:36 -0500

    updated html pages and css

--- src/main/java/com/example/CENProj/controller/SeedStarterMngController.java
+++ src/main/java/com/example/CENProj/controller/SeedStarterMngController.java
@@ -3,10 +3,10 @@ package com.example.CENProj.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Calendar;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -33,4 +33,55 @@ public class SeedStarterMngController {
         model.clear();
         return "redirect:/seedstartermng";
     }
+
+    @RequestMapping("/about-us")
+    public String showAboutUs() {
+        return "about-us";
+    }
+
+    @RequestMapping("/community-forums")
+    public String showCommunityForums() {
+        return "community-forums";
+    }
+
+    @RequestMapping("/account")
+    public String showAccount() {
+        return "account";
+    }
+
+    @RequestMapping("/find-therapists")
+    public String showFindTherapists() {
+        return "find-therapists";
+    }
+
+    @RequestMapping("/therapy-portal")
+    public String showTherapyPortal() {
+        return "therapy-portal";
+    }
+
+    @RequestMapping("/mood-tracker")
+    public String showMoodTracker() {
+        return "mood-tracker";
+    }
+
+    @RequestMapping("/therapy-exercises")
+    public String showTherapyExercises() {
+        return "therapy-exercises";
+    }
+
+    @RequestMapping("/meditation-guides")
+    public String showMeditationGuides() {
+        return "meditation-guides";
+    }
+
+    @RequestMapping("/crisis-management-resources")
+    public String showCrisisManagementResources() {
+        return "crisis-management-resources";
+    }
+
+    @RequestMapping("/faq")
+    public String showFaq() {
+        return "faq";
+    }
+
 }
