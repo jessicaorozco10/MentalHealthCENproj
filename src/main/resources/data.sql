a19eebc56434ff1eb5222e06346329c26cb16e92
jessicaorozco10 <jessicaorozco10@gmail.com>
2025-04-15 15:45:10 -0400

    Add data.sql for startup of default users from the group

---
+++ src/main/resources/data.sql
@@ -0,0 +1,21 @@
+insert or IGNORE into users (id, first_name, last_name, password, email, publication_date, user_type)
+values (1, 'Jessica', 'Orozco', '$2a$10$b.SaKCxJI0F5.JDppThxEOKqdTRnt4fsnnFxqITWaaqXtoA2EtIry',
+        'Jessica.Orozco@fiu.edu', 1744516800000, 1);
+insert or IGNORE into users (id, first_name, last_name, password, email, publication_date, user_type)
+values (2, 'Aaron', 'Caicedo', '$2a$10$b.SaKCxJI0F5.JDppThxEOKqdTRnt4fsnnFxqITWaaqXtoA2EtIry',
+        'Aaron.Caicedo@fiu.edu', 1744516800000, 1);
+insert or IGNORE into users (id, first_name, last_name, password, email, publication_date, user_type)
+values (3, 'Volreka', 'Senatus', '$2a$10$b.SaKCxJI0F5.JDppThxEOKqdTRnt4fsnnFxqITWaaqXtoA2EtIry',
+        'Volreka.Senatus@fiu.edu', 1744516800000, 1);
+insert or IGNORE into users (id, first_name, last_name, password, email, publication_date, user_type)
+values (4, 'Gaeldesh', 'Benuit', '$2a$10$b.SaKCxJI0F5.JDppThxEOKqdTRnt4fsnnFxqITWaaqXtoA2EtIry',
+        'Gaeldesh.Benuit@fiu.edu', 1744516800000, 1);
+insert or IGNORE into users (id, first_name, last_name, password, email, publication_date, user_type)
+values (5, 'Logan', 'Atty', '$2a$10$b.SaKCxJI0F5.JDppThxEOKqdTRnt4fsnnFxqITWaaqXtoA2EtIry',
+        'Logan.Atty@fiu.edu', 1744516800000, 1);
+insert or IGNORE into users (id, first_name, last_name, password, email, publication_date, user_type)
+values (6, 'Daniel', 'Herrera', '$2a$10$b.SaKCxJI0F5.JDppThxEOKqdTRnt4fsnnFxqITWaaqXtoA2EtIry',
+        'Daniel.Herrera@fiu.edu', 1744516800000, 1);
+insert or IGNORE into users (id, first_name, last_name, password, email, publication_date, user_type)
+values (7, 'Andres', 'Hernandez', '$2a$10$b.SaKCxJI0F5.JDppThxEOKqdTRnt4fsnnFxqITWaaqXtoA2EtIry',
+        'Andres.Hernandez@fiu.edu', 1744516800000, 1);
